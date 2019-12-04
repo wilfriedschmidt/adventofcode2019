62dbf5d27e57ef0d179b8a05d9bf3a394386427f
korben dallas <wtschmidt@gmail.com>
2019-12-03 21:37:43 -0800

    day4

---
+++ src/day4.rs
@@ -0,0 +1,62 @@
+pub fn go()
+{
+  let start:i32 = 125730;
+  let end:i32 = 579381;
+
+  let mut countpart1 = 0;
+  let mut countpart2 = 0;
+
+  for _i in start..end
+  {
+    let mut haspair = false;
+    let mut isincreasing = true;
+    let mut atleastonepair = false;
+
+    let mut haspaircount = 0;
+    
+    let mut lastdigit:i32 = -1;
+    let mut temp = _i;
+
+    for _j in 0..6
+    {
+      let digit = temp%10;
+      temp = temp/10;
+
+      if lastdigit==-1
+      {
+        lastdigit = digit;
+      }
+      else if digit==lastdigit
+      {
+        haspair = true;
+        haspaircount+=1;
+      }
+      else
+      {
+        if haspaircount==1
+        {
+          atleastonepair = true
+        }
+        haspaircount=0;
+      }
+
+      if digit>lastdigit
+      {
+        isincreasing = false;
+      }
+
+      lastdigit = digit;    
+    }
+
+    if isincreasing && haspair
+    {
+      countpart1+=1;
+    }
+    if isincreasing && (atleastonepair || haspaircount==1)
+    {
+      countpart2+=1;
+    }
+  }
+
+  println!("{} {}",countpart1, countpart2);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod util;
-mod grid;
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 use std::env;
 
@@ -15,7 +15,7 @@ fn main()
     println!("enter day...");
   } 
   else
-  { 
+  {
     if args[1]=="day1" || args[1]=="all"
     {
       day1::go("./data/day1_input.txt");
@@ -29,6 +29,11 @@ fn main()
     if args[1]=="day3" || args[1]=="all"
     {
       day3::go("./data/day3_input.txt");
+    }
+
+    if args[1]=="day4" || args[1]=="all"
+    {
+      day4::go();
     }
   }
 }
